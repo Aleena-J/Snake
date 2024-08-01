ee3cf6566b0ff7218d3a183b202d0386eefee4b2
Aleena James <aleena2003james@gmail.com>
2024-08-01 13:51:49 -0400

    Added game functionality: Snake movement, collision detection, score/apple detection, game speeding up after collection apples and addition of more body parts. Also made the game bigger, and closes upon collision.

--- src/snakeGame/Game.java
+++ src/snakeGame/Game.java
@@ -9,14 +9,17 @@ import java.io.File;
 import java.io.IOException;
 
 public class Game extends JPanel implements KeyListener, ActionListener {
-    final private int gameWidth = 600;
-    final private int gameHeight = 600;
-    final private int screenHeight = 700; //Add 100 pixels for score count at bottom
-    final private int cellSize = 50; //Width/Length of each cell on board(grid)
+    final private int gameWidth = 700;
+    final private int gameHeight = 700;
+    final private int screenHeight = 800; //Add 100 pixels for score count at bottom
+    final private int cellSize = 35; //Width/Length of each cell on board(grid)
     private boolean gameStarted;
     private int score;
     private Snake snake;
     private Apple apple;
+    private JFrame frame;
+    private Timer timer;
+    private boolean directionChanged;
 
     public Game(){
         this.gameStarted = false;
@@ -24,7 +27,7 @@ public class Game extends JPanel implements KeyListener, ActionListener {
     }
 
     public void display(){
-        JFrame frame = new JFrame("Snake Game");
+        frame = new JFrame("Snake Game");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.getContentPane().setBackground(Color.BLACK);
         frame.setResizable(false);
@@ -42,11 +45,14 @@ public class Game extends JPanel implements KeyListener, ActionListener {
     }
 
     public void startGame(){
-        this.gameStarted = true;
         this.score = 0;
         this.snake = new Snake(gameWidth, gameHeight, cellSize);
         this.apple = new Apple();
-        apple.generate(gameWidth, gameHeight, cellSize);
+        gameStarted = true;
+        apple.generate(gameWidth, gameHeight, cellSize); //Generate apple location
+        timer = new Timer(snake.getSpeed(), this);
+        timer.start();
+        directionChanged = false;
     }
 
     public void draw(Graphics graphic){
@@ -59,36 +65,93 @@ public class Game extends JPanel implements KeyListener, ActionListener {
                 graphic.drawLine(i*cellSize, 0, i*cellSize, gameHeight);
             }
 
+            //Draw snake
+            for(int i = 0; i < snake.getNumParts(); i++){
+                if(i == 0){
+                    graphic.setColor(new Color(18, 165, 43));
+                    graphic.fillRect(snake.getX(i), snake.getY(i), cellSize, cellSize);
+                }else if(i % 2 == 0) {
+                    graphic.setColor(Color.GREEN);
+                    graphic.fillRect(snake.getX(i), snake.getY(i), cellSize, cellSize);
+                }else{
+                    graphic.setColor(Color.BLUE);
+                    graphic.fillRect(snake.getX(i), snake.getY(i), cellSize, cellSize);
+                }
+            }
+
             //Draw apple
             graphic.setColor(Color.RED);
             graphic.fillOval(apple.getAppleXCoord(), apple.getAppleYCoord(), cellSize, cellSize);
 
-            //Draw snake
-            for(int i = 0; i < snake.getNumParts(); i++){
-                graphic.setColor(Color.GREEN);
-                graphic.fillRect(snake.getX(i), snake.getY(i), cellSize, cellSize);
-            }
-
             //Draw scoreboard
             graphic.setColor(Color.WHITE);
             graphic.setFont(new Font("Papyrus", Font.BOLD, 48));
-            graphic.drawString("Score: " + score, 0, 675);
+            graphic.drawString("Score: " + score, gameWidth/3, gameHeight + 75);
 
         }else{
-
+            timer.stop();
+            frame.dispose();
         }
 
     }
+
+
 
     public void paintComponent(Graphics graphic) {
         super.paintComponent(graphic);
         draw(graphic);
     }
 
+    public boolean checkCollision(){
+        //Check if snake head collides with rest of body
+        for(int i = 1; i < snake.getNumParts(); i++){
+            if((snake.getX(i) == snake.getX(0)) && (snake.getY(i) == snake.getY(0))){
+                return true;
+            }
+        }
+
+        //Check if snake head collides with screen borders
+        if(snake.getY(0) < 0 || snake.getY(0) >= gameHeight){
+            return true;
+        }
+
+        if(snake.getX(0) < 0 || snake.getX(0) >= gameWidth){
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean checkAppleCollision(){
+        if(snake.getX(0) == apple.getAppleXCoord() && snake.getY(0) == apple.getAppleYCoord()){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        directionChanged = false;
+        //Move the snake
+        snake.move(cellSize);
+        //Check for an apple
+        if(checkAppleCollision()){
+            apple.generate(gameWidth, gameHeight, cellSize);
+            score++;
+            snake.addPart();
+            //Every 5 apple, make the snake go faster
+            if(score % 5 == 0) {
+                snake.adjustSpeed();
+            }
+            timer.setDelay(snake.getSpeed());
+        }
+        //Check for collision
+        if(checkCollision()){
+            gameStarted = false;
+        }
 
+        repaint();
     }
 
     @Override
@@ -98,27 +161,33 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        switch(e.getKeyCode()) {
-            case KeyEvent.VK_LEFT:
-                if(snake.getDirection() != 'R') {
-                    snake.setDirection('L');
-                }
-                break;
-            case KeyEvent.VK_RIGHT:
-                if(snake.getDirection() != 'L') {
-                    snake.setDirection('R');
-                }
-                break;
-            case KeyEvent.VK_UP:
-                if(snake.getDirection() != 'D') {
-                    snake.setDirection('U');
-                }
-                break;
-            case KeyEvent.VK_DOWN:
-                if(snake.getDirection() != 'U') {
-                    snake.setDirection('D');
-                }
-                break;
+        if(!directionChanged) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    if (snake.getDirection() != 'R') {
+                        snake.setDirection('L');
+                        directionChanged = true;
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if (snake.getDirection() != 'L') {
+                        snake.setDirection('R');
+                        directionChanged = true;
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if (snake.getDirection() != 'D') {
+                        snake.setDirection('U');
+                        directionChanged = true;
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if (snake.getDirection() != 'U') {
+                        snake.setDirection('D');
+                        directionChanged = true;
+                    }
+                    break;
+            }
         }
     }
 
--- src/snakeGame/Snake.java
+++ src/snakeGame/Snake.java
@@ -8,11 +8,13 @@ public class Snake {
     private int[] yParts; //Y location of body parts
 
     public Snake(int gameWidth, int gameHeight, int cellSize){
-        this.speed = 25;
+        this.speed = 125;
         this.direction = 'R';
         this.numParts = 3;
         this.xParts = new int[(gameWidth*gameHeight) / (cellSize * cellSize)];
         this.yParts = new int[(gameWidth*gameHeight) / (cellSize * cellSize)];
+        this.xParts[0] = ((gameWidth/cellSize) / 2) * cellSize;
+        this.yParts[0] = ((gameHeight/cellSize) / 2) * cellSize;
     }
 
     public char getDirection(){
@@ -33,6 +35,44 @@ public class Snake {
 
     public int getY(int i){
         return yParts[i];
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+
+    public void move(int cellSize){
+        //Make body parts move into location of body part ahead of it
+        for(int i = numParts - 1; i > 0; i--){
+            xParts[i] = xParts[i - 1];
+            yParts[i] = yParts[i - 1];
+        }
+
+        //Depending on direction key, make the snake head move by one grid unit
+        if(direction == 'R'){
+            xParts[0] = xParts[0] + cellSize;
+        }
+        else if(direction == 'L'){
+            xParts[0] = xParts[0] - cellSize;
+        }else if(direction == 'U'){
+            yParts[0] = yParts[0] - cellSize;
+        }else if(direction == 'D'){
+            yParts[0] = yParts[0] + cellSize;
+        }
+    }
+
+    public void addPart(){
+        xParts[numParts] = xParts[numParts - 1];
+        yParts[numParts] = yParts[numParts - 1];
+        numParts++;
+    }
+
+    public void adjustSpeed(){
+        if(speed > 40) {
+            speed = speed - 5;
+        }else{
+            speed = 40;
+        }
     }
 
 }
